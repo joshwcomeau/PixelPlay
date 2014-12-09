cd7df7ef698a2df238c2a439e207669a92cd44f2
Josh Comeau <joshwcomeau@gmail.com>
2014-12-09 09:08:52 -0500

    YES! Got authorization working. callback.html populated with the not-mentioned-anywhere JS to populate the opener with its data

--- public/app.js
+++ public/app.js
@@ -1,16 +1,16 @@
 angular
   .module('pixelPlay', ['ngRoute', 'ngResource', 'appRoutes', 'pixelPlay.game', 'pixelPlay.dashboard'])
-  .run(function() {
+  .run(['User', function(User) {
     _500px.init({
       sdk_key: '1e6cd00470800d39b07106a70a650cdf88277901'
     });
 
     console.log("500px initialized");
 
-    // _500px.getAuthorizationStatus();
+    _500px.getAuthorizationStatus();
 
-    // _500px.on('authorization_obtained', function () {
-    //   console.log("Authorization has been obtained, running");
-    //   User.update_user('authorized');
-    // });
-  });
+    _500px.on('authorization_obtained', function () {
+      console.log("Authorization has been obtained, running");
+      User.update_user('authorized');
+    });
+  }]);
--- public/components/dashboard/dashboard.ctrl.js
+++ public/components/dashboard/dashboard.ctrl.js
@@ -1,9 +1,16 @@
 function DashboardController($scope, User) {
   this.tagline = "Welcome to the dashboard.";
-  this.login = _500px.login(User.update_user);
+  this.userService = User;
 }
 
-DashboardController.prototype.someMethod = function() {};
+DashboardController.prototype.login = function() {
+  console.log("Login clicked");
+  _500px.login(this.userService.update_user);
+};
+
+DashboardController.prototype.getAuthStatus = function() {
+  alert(_500px.getAuthorizationStatus());
+};
 
 DashboardController.$inject = ['$scope', 'User'];
 angular.module('pixelPlay.dashboard').controller('DashboardController', ['$scope', 'User', DashboardController]);
