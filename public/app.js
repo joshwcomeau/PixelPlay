dfc0f95ba1aae4a0753a0bbdc56bb20af3a5c819
Josh Comeau <joshwcomeau@gmail.com>
2014-12-09 08:25:04 -0500

    Fixed a few tiny things (missing semicolons mostly)

--- public/app.js
+++ public/app.js
@@ -5,9 +5,12 @@ angular
       sdk_key: '1e6cd00470800d39b07106a70a650cdf88277901'
     });
 
+    console.log("500px initialized");
+
     _500px.getAuthorizationStatus();
 
     _500px.on('authorization_obtained', function () {
+      console.log("Authorization has been obtained, running");
       User.update_user('authorized');
     });
   });
--- public/components/user/user.service.js
+++ public/components/user/user.service.js
@@ -4,11 +4,13 @@ angular.module('pixelPlay').service('User', function() {
   // This is passed in as the callback to 500px's login().
   // 'res' is a simple string letting us know if they've been approved or not.
   this.update_user = function(res) {
+    console.log("Updating user");
     if ( res == 'authorized' ) {
+      console.log("user is authorized!");
       // Cool, we've got their permission. Let's get their username.
       _500px.api('/users', function (response) {
         console.log(response);
       });
     }
-  }
+  };
 });
