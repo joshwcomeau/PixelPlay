575ae371f7c6bad1f03674a8aaaae0a82a162837
Joshua Comeau <joshwcomeau@gmail.com>
2014-12-10 07:24:31 -0500

    Expanded the Photo GET request, filtered out photos that don't have lat/lng, and added a method to pop photos from the array. Still needs to be extracted into a service though.

--- public/components/game/game.ctrl.js
+++ public/components/game/game.ctrl.js
@@ -1,18 +1,38 @@
-function GameController($scope, FetchPhotos) {
+function GameController($scope) {
   this.round = 1;
-  this.photos = null;
+  this.photoData = null;
+  this.currentPhoto = null;
 }
 
 GameController.prototype.start = function() {
+  var game = this;
+
   // get photos. Will extract to Service if it works
-  _500px.api('/photos', {feature: 'popular', page: 1}, function (response) {
+  var photo_options = {
+    feature:    'fresh_today',
+    only:       'Urban Exploration',
+    image_size: 4,
+    rpp:        100
+  };
+
+  _500px.api('/photos', photo_options, function (response) {
     if (response.success) {
-      alert('There are ' + response.data.photos.length + ' photos.');
+      console.log(response.data)
+      game.photoData = _.filter(response.data.photos, function(photo) {
+        return photo.latitude !== null;
+      });
+      game.currentPhoto = game.photoData.pop()
+
+      console.log("num of photos", response.data.photos.length, "after filtering", game.photoData.length)
     } else {
       alert('Unable to complete request: ' + response.status + ' - ' + response.error_message);
     }
   });
 };
 
-GameController.$inject = ['$scope', 'FetchPhotos'];
-angular.module('pixelPlay.game').controller('GameController', ['$scope', 'FetchPhotos', GameController]);
+GameController.prototype.getNextPhoto = function() {
+  this.currentPhoto = this.photoData.pop()
+};
+
+GameController.$inject = ['$scope'];
+angular.module('pixelPlay.game').controller('GameController', ['$scope', GameController]);
